a71b9f164e676aa965d52a77c1f09596d993b03f
kshamajoglekar <kshama.shepal@gmail.com>
2020-09-08 21:15:19 -0400

    Add longest palindrom

---
+++ src/main/java/com/ks/leetcode/LongestSubstringPalindrom.java
@@ -0,0 +1,22 @@
+package com.ks.leetcode;
+
+class LongestSubstringPalindrom {
+    public static void main(String[] args) {
+
+        System.out.println("Hi Sia ... How are your4ui87yfikot3ehyuryfdty5i");
+    }
+
+    public String longestPalindrome() {
+
+        String s = "abbskkgsdfkjlsidfshhjsodfj";
+
+
+        //TODO change it
+        return "-1";
+    }
+
+}
+
+
+
+
